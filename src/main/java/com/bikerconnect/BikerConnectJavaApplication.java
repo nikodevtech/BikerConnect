3f50881ef6e17b9f37c412245475a2f911a83e1a
niko <nikoalvarezzapata@gmail.com>
2024-01-19 15:52:27 +0100

    estructura inicial basica proyecto

---
+++ src/main/java/com/bikerconnect/BikerConnectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bikerconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerConnectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikerConnectJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikerconnect/BikerConnectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikerConnectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
