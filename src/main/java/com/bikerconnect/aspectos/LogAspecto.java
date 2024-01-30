364e7423b4842b3a1095c8d407f28041875ff7eb
nico <nikoalvarezzapata@gmail.com>
2024-01-30 09:54:30 +0100

    feature fichero log con AOP

---
+++ src/main/java/com/bikerconnect/aspectos/LogAspecto.java
@@ -0,0 +1,50 @@
+package com.bikerconnect.aspectos;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ *  Aspecto para realizar un seguimiento de las acciones en la app. La programación orientada aspectos (AOP)
+ *  permite modularizar el código para encapsular tareas repetitivas transversales separandolas de la 
+ *  lógica de negocio. Para este caso de control del log se utilizará la librería SLF4J.
+ */
+@Aspect
+@Component
+public class LogAspecto {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	
+	// Registrar en el fichero log despues de lanzar cualquier excepción	
+	// En estos caso el pointcut es cualquier metodo o clase de la app
+	@AfterThrowing(pointcut = "execution(* com.bikerconnect..*.*(..))", throwing = "ex")
+	public void logException(JoinPoint joinPoint, Throwable ex) {
+		logger.error("Error en el método {}() de la clase {}: {}", 
+				joinPoint.getSignature().getName(),
+				joinPoint.getTarget().getClass().getName(),
+				ex.getMessage());
+	}
+	
+	// Registrar en el fichero log la entrada a métodos
+    @Before("execution(* com.tu.paquete..*.*(..))")
+    public void logMetodoEntrada(JoinPoint joinPoint) {
+        logger.info("Entrando en el método {}() de la clase {}", 
+                     joinPoint.getSignature().getName(),
+                     joinPoint.getTarget().getClass().getName());
+    }
+
+    // Registrar en el fichero log la salida de métodos (después de la ejecución exitosa)
+    @AfterReturning("execution(* com.bikerconnect..*.*(..))")
+    public void logMethodoSalida(JoinPoint joinPoint) {
+        logger.info("Saliendo del método {}() de la clase {}", 
+                     joinPoint.getSignature().getName(),
+                     joinPoint.getTarget().getClass().getName());
+    }
+
+}
