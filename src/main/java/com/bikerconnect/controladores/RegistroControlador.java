1cac6e8d0c44115bae3834e47b92fd0e72fa7207
niko <nikoalvarezzapata@gmail.com>
2024-02-03 15:00:17 +0100

    cambios registro normal y admin

--- src/main/java/com/bikerconnect/controladores/RegistroControlador.java
+++ src/main/java/com/bikerconnect/controladores/RegistroControlador.java
@@ -1,6 +1,7 @@
 package com.bikerconnect.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,61 +18,67 @@ import com.bikerconnect.servicios.IUsuarioServicio;
 @Controller
 public class RegistroControlador {
 
-    @Autowired
-    private IUsuarioServicio usuarioServicio;
+	@Autowired
+	private IUsuarioServicio usuarioServicio;
 
-    /**
-     * Gestiona la solicitud HTTP GET para la url /auth/registrar y muestra la página de registro.
-     *
-     * @param model Modelo que se utiliza para enviar un usuarioDTO vacío a la vista.
-     * @return La vista de registro de usuario (registro.html).
-     */
-    @GetMapping("/auth/crear-cuenta")
-    public String registrarGet(Model model) {
-        try {
-            model.addAttribute("usuarioDTO", new UsuarioDTO());
-            return "registro";
-        } catch (Exception e) {
-            model.addAttribute("error", "Error al procesar la solicitud. Por favor, inténtelo de nuevo.");
-            return "registro";
-        }
-    }
+	/**
+	 * Gestiona la solicitud HTTP GET para la url /auth/registrar y muestra la
+	 * página de registro.
+	 *
+	 * @param model Modelo que se utiliza para enviar un usuarioDTO vacío a la
+	 *              vista.
+	 * @return La vista de registro de usuario (registro.html).
+	 */
+	@GetMapping("/auth/crear-cuenta")
+	public String registrarGet(Model model) {
+		try {
+			model.addAttribute("usuarioDTO", new UsuarioDTO());
+			return "registro";
+		} catch (Exception e) {
+			model.addAttribute("error", "Error al procesar la solicitud. Por favor, inténtelo de nuevo.");
+			return "registro";
+		}
+	}
 
-    /**
-     * Procesa la solicitud HTTP POST para registro de un nuevo usuario.
-     *
-     * @param usuarioDTO El objeto UsuarioDTO que recibe en el modelo y contiene los
-     *                   datos del nuevo usuario.
-     * @param model      Modelo que se utiliza para enviar mensajes a la vista.
-     * @return La vista de inicio de sesión (login.html) si fue exitoso el registro;
-     * de lo contrario, la vista de registro de usuario (registro.html).
-     */
-    @PostMapping("/auth/crear-cuenta")
-    public String registrarPost(@ModelAttribute UsuarioDTO usuarioDTO, Model model) {
-        try {
-            UsuarioDTO nuevoUsuario = usuarioServicio.registrarUsuario(usuarioDTO);
+	/**
+	 * Procesa la solicitud HTTP POST para registro de un nuevo usuario.
+	 *
+	 * @param usuarioDTO El objeto UsuarioDTO que recibe en el modelo y contiene los
+	 *                   datos del nuevo usuario.
+	 * @param model      Modelo que se utiliza para enviar mensajes a la vista.
+	 * @return La vista de inicio de sesión (login.html) si fue exitoso el registro;
+	 *         de lo contrario, la vista de registro de usuario (registro.html).
+	 */
+	@PostMapping("/auth/crear-cuenta")
+	public String registrarPost(@ModelAttribute UsuarioDTO usuarioDTO, Model model, Authentication authentication) {
+		try {
 
-            if (nuevoUsuario == null) {
-				model.addAttribute("usuarioRegistradoPeroNoConfirmado", "Ya existe un usuario con ese email sin confirmar");
+			UsuarioDTO nuevoUsuario = usuarioServicio.registrarUsuario(usuarioDTO);
+
+			String username = "";
+			if (authentication != null && authentication.isAuthenticated()) {
+				username = authentication.getName();
+			}
+			
+			if (nuevoUsuario == null) {
+				model.addAttribute("usuarioRegistradoPeroNoConfirmado",
+						"Ya existe un usuario con ese email sin confirmar");
 				return "registro";
-			} else if(nuevoUsuario.getMensajeError().equals("Usuario ya registrado y confirmado")) {
-                // De lo contrario, es que ya existe un usuario con el dicho email
-                model.addAttribute("emailYaRegistrado", "Ya existe un usuario con ese email");
-                return "registro";
-            } else if (nuevoUsuario != null && !nuevoUsuario.isCuentaConfirmada()) {
-                // Si entra a este if es que el registro se completó correctamente
-                model.addAttribute("mensajeRegistroExitoso", "Registro del nuevo usuario OK");
-                return "login";
-            } else if (nuevoUsuario.isCuentaConfirmada()) {
-                model.addAttribute("mensajeRegistroExitoso", "Registro del nuevo usuario OK");
-                model.addAttribute("usuarios", usuarioServicio.obtenerTodos());
-                return "administracionUsuarios";
-            } 
-        } catch (Exception e) {
-            model.addAttribute("error", "Error al procesar la solicitud. Por favor, inténtelo de nuevo.");
-            return "registro";
-        }
-        return "registro";
-    }
+			} else if (nuevoUsuario.getMensajeError().equals("Usuario ya registrado y confirmado")) {
+				model.addAttribute("emailYaRegistrado", "Ya existe un usuario con ese email");
+				return "registro";
+			} else if (nuevoUsuario != null && !username.contains("admin")) {
+				model.addAttribute("mensajeRegistroExitoso", "Registro del nuevo usuario OK");
+				return "login";
+			} else {
+				model.addAttribute("mensajeRegistroExitoso", "Registro del nuevo usuario OK");
+				model.addAttribute("usuarios", usuarioServicio.obtenerTodos());
+				return "administracionUsuarios";
+			}
+		} catch (Exception e) {
+			model.addAttribute("error", "Error al procesar la solicitud. Por favor, inténtelo de nuevo.");
+			return "registro";
+		}
+	}
 
 }
