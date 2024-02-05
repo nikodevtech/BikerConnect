52b53ec6e907a6a82e7febcc54634b613f89940e
niko <nikoalvarezzapata@gmail.com>
2024-02-05 12:38:05 +0100

    agregado datos de quedadas en data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -38,20 +38,44 @@ VALUES
 INSERT INTO gestion_logicanegocio.motos (id_usuario_propietario, marca, modelo, año, color, desc_modificaciones)
 VALUES
 (2, 'Yamaha', 'YZF-R6', 2018, 'Negro', 'Escape Yoshimura, Cúpula Zero Gravity'),
-(2, 'Kawasaki', 'Ninja ZX-10R', 2021, 'Verde', 'Suspensión Showa, Frenos Brembo'),
-(3, 'Suzuki', 'GSX-R750', 2019, 'Azul', 'Escape Akrapovic, Amortiguador Öhlins'),
-(3, 'Ducati', 'Panigale V4', 2020, 'Rojo', 'Suspensión Öhlins, Frenos Brembo'),
-(4, 'Honda', 'CBR1000RR', 2022, 'Blanco', 'Escape Yoshimura, Cúpula Puig'),
-(4, 'BMW', 'S1000RR', 2021, 'Gris', 'Suspensión Ohlins, Quickshifter'),
-(5, 'Kawasaki', 'Ninja 650', 2019, 'Negro', 'Escape Akrapovic, Cúpula Puig'),
-(5, 'Triumph', 'Street Triple RS', 2020, 'Blanco', 'Suspensión Öhlins, Frenos Brembo'),
-(6, 'Yamaha', 'MT-09', 2021, 'Azul', 'Escape Akrapovic, Cúpula Puig'),
-(6, 'Ducati', 'Monster 821', 2018, 'Rojo', 'Suspensión Öhlins, Frenos Brembo'),
-(7, 'Suzuki', 'SV650', 2020, 'Negro', 'Escape Yoshimura, Cúpula Puig'),
-(7, 'KTM', 'Duke 790', 2019, 'Naranja', 'Suspensión WP, Frenos Brembo'),
-(8, 'Honda', 'CB500F', 2022, 'Blanco', 'Escape Akrapovic, Cúpula Puig'),
-(8, 'Harley-Davidson', 'Street Rod', 2021, 'Negro', 'Asiento Biplaza, Alforjas'),
-(9, 'Kawasaki', 'Z900', 2019, 'Verde', 'Escape Akrapovic, Cúpula Puig'),
-(9, 'MV Agusta', 'Brutale 800', 2020, 'Rojo', 'Suspensión Öhlins, Frenos Brembo'),
-(10, 'Ducati', 'Scrambler Icon', 2021, 'Amarillo', 'Escape Termignoni, Manillar Rizoma'),
-(10, 'BMW', 'R1250GS', 2022, 'Gris', 'Suspensión ESA, Asientos Calefactables');
+(3, 'Kawasaki', 'Ninja ZX-10R', 2021, 'Verde', 'Suspensión Showa, Frenos Brembo'),
+(4, 'Suzuki', 'GSX-R750', 2019, 'Azul', 'Escape Akrapovic, Amortiguador Öhlins'),
+(5, 'Ducati', 'Panigale V4', 2020, 'Rojo', 'Suspensión Öhlins, Frenos Brembo'),
+(6, 'Honda', 'CBR1000RR', 2022, 'Blanco', 'Escape Yoshimura, Cúpula Puig'),
+(7, 'BMW', 'S1000RR', 2021, 'Gris', 'Suspensión Ohlins, Quickshifter'),
+(8, 'Kawasaki', 'Ninja 650', 2019, 'Negro', 'Escape Akrapovic, Cúpula Puig'),
+(9, 'Triumph', 'Street Triple RS', 2020, 'Blanco', 'Suspensión Öhlins, Frenos Brembo'),
+(10, 'Yamaha', 'MT-09', 2021, 'Azul', 'Escape Akrapovic, Cúpula Puig'),
+(16, 'Ducati', 'Monster 821', 2018, 'Rojo', 'Suspensión Öhlins, Frenos Brembo'),
+(11, 'Suzuki', 'SV650', 2020, 'Negro', 'Escape Yoshimura, Cúpula Puig'),
+(12, 'KTM', 'Duke 790', 2019, 'Naranja', 'Suspensión WP, Frenos Brembo'),
+(15, 'Honda', 'CB500F', 2022, 'Blanco', 'Escape Akrapovic, Cúpula Puig'),
+(13, 'Harley-Davidson', 'Street Rod', 2021, 'Negro', 'Asiento Biplaza, Alforjas'),
+(14, 'Kawasaki', 'Z900', 2019, 'Verde', 'Escape Akrapovic, Cúpula Puig'),
+(14, 'MV Agusta', 'Brutale 800', 2020, 'Rojo', 'Suspensión Öhlins, Frenos Brembo'),
+(15, 'Ducati', 'Scrambler Icon', 2021, 'Amarillo', 'Escape Termignoni, Manillar Rizoma'),
+(16, 'BMW', 'R1250GS', 2022, 'Gris', 'Suspensión ESA, Asientos Calefactables');
+
+INSERT INTO gestion_logicanegocio.quedadas (desc_quedada, fch_hora_encuentro, lugar, estado, usuario_organizador)
+VALUES
+('Conocer personas cerca de San Sebastián', '2024-03-10 13:00:00', 'San Sebastián, aparcamientos playa Ondarreta', 'Planificada', 'juan.perez@example.com');
+
+INSERT INTO gestion_logicanegocio.participantes (id_quedada, id_usuario)
+VALUES
+(1, 2), (1, 3), (1, 4), (1, 5),
+(1, 6), (1, 7), (1, 8), (1, 9),
+(1, 10), (1, 11), (1, 12), (1, 13),
+(1, 14), (1, 15);
+
+INSERT INTO gestion_logicanegocio.quedadas (desc_quedada, fch_hora_encuentro, lugar, estado, usuario_organizador)
+VALUES
+('Conocer personas cerca de Sevilla', '2024-02-11 16:00:00', 'Sevilla, estadio olimpico', 'Cancelada', 'maria.lopez@example.com');
+
+INSERT INTO gestion_logicanegocio.quedadas (desc_quedada, fch_hora_encuentro, lugar, estado, usuario_organizador)
+VALUES
+('Quedada para conocer personas cerca de Jerez', '2024-02-12 17:00:00', 'Jerez, entrada circuito', 'Completada', 'carlos.sanchez@example.com');
+
+INSERT INTO gestion_logicanegocio.participantes (id_quedada, id_usuario)
+VALUES
+(3, 2), (3, 3), (3, 4), (3, 5),
+(3, 6), (3, 7), (3, 8), (3, 9);
